711fea246419b48da1d44f19b04787b123f91c66
Petre Diana <petre.diana98@yahoo.ro>
2020-05-03 15:58:45 +0300

    added vue bootstrap and header comp

---
+++ frontend-vue-interface/trillo-copy/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>Trillo copy :)</b></b-nav-item>
+            <b-nav-item disabled>Boards</b-nav-item>
+        </b-nav>
+    </div>
+</template>
