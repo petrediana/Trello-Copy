bd5baf4c607806f2ecdef702e7a030b33ccaa36a
Petre Diana <petre.diana98@yahoo.ro>
2020-05-03 11:27:30 +0300

    added in app some error handling and the user route

--- backend-server/app.js
+++ backend-server/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const routes = require('./api/routes');
 
 mongoose.connect('mongodb+srv://diana:'
 + process.env.MONGO_ATLAS_PW
@@ -15,6 +16,23 @@ mongoose.connect('mongodb+srv://diana:'
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use('/user-api/users', routes.user);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found :(');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message : error.message
+        }
+    });
+});
 
 
 module.exports = app;
