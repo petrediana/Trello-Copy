01f5798988d4d91cf8785c13b40c7025dfe5a6b6
Petre Diana <petre.diana98@yahoo.ro>
2020-05-03 19:27:46 +0300

    added margin for login component

--- frontend-vue-interface/trillo-copy/src/components/Header.vue
+++ frontend-vue-interface/trillo-copy/src/components/Header.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
+    <div class="header-container">
         <b-nav tabs>
             <b-nav-item disabled><b>Trillo copy :)</b></b-nav-item>
+            <b-nav-item disabled>ToDo: maybe change this</b-nav-item>
             <b-nav-item disabled>Created by Petre Diana</b-nav-item>
         </b-nav>
 
     </div>
 </template>
+
+<style scoped>
+    .header-container {
+        margin-bottom: 2rem 
+    }
+</style>
