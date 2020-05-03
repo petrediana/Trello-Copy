ab56f5131a04505bbfaecb852e93963a102a2d75
Petre Diana <petre.diana98@yahoo.ro>
2020-05-03 11:15:06 +0300

    added nodemon.json and connection to mongodb atlas

--- backend-server/app.js
+++ backend-server/app.js
@@ -4,6 +4,14 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://diana:'
++ process.env.MONGO_ATLAS_PW
++ '@node-rest-trillo-copy-fwtqb.mongodb.net/test?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
