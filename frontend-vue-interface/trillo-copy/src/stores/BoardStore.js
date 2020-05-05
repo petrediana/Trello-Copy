0abb1aa563332d8be97b1075c5d8e899b6512ba9
Petre Diana <petre.diana98@yahoo.ro>
2020-05-05 10:10:00 +0300

    added function to get starred boards

--- frontend-vue-interface/trillo-copy/src/stores/BoardStore.js
+++ frontend-vue-interface/trillo-copy/src/stores/BoardStore.js
@@ -2,7 +2,8 @@ const SERVER = 'http://localhost:8081/board-api/boards'
 
 class BoardStore {
     constructor() {
-        this.userAllBoardsFromDb = []
+        this.userAllBoardsFromDb = [],
+        this.userAllStarredBoardsFromDb = []
     }
 
     async getAllBoardsForUser(userId) {
@@ -15,6 +16,23 @@ class BoardStore {
                     _id: userBoard._id,
                     starred: userBoard.starred,
                     description: userBoard.description
+                })
+            })
+        } catch(err) {
+            console.warn(err)
+        }
+    }
+
+    async getAllStarredBoardsForUser(userId) {
+        try {
+            const request = await fetch(`${SERVER}/starred/${userId}`)
+            const response = await request.json();
+
+            response.forEach(userStarredBoard => {
+                this.userAllStarredBoardsFromDb.push({
+                    _id: userStarredBoard._id,
+                    starred: userStarredBoard.starred,
+                    description: userStarredBoard.description
                 })
             })
         } catch(err) {
