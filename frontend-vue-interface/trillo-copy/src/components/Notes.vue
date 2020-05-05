f864b5d9952fea800779f0fbf532b67390701fbc
Petre Diana <petre.diana98@yahoo.ro>
2020-05-05 17:12:38 +0300

    trying to set a popup

--- frontend-vue-interface/trillo-copy/src/components/Notes.vue
+++ frontend-vue-interface/trillo-copy/src/components/Notes.vue
@@ -3,23 +3,36 @@
         <b-list-group-item button variant="info"
         v-for="(note, index) in this.notes"
         :key="index"
+        @focus="handleClick(note._id)"
+        @focusout="handleOutsideClick()"
         >
             {{ note.name }}
         </b-list-group-item>
+
+        <div v-if="isNoteClicked">
+            <Popup />
+        </div>
     </div>
+
 </template>
 
 <script>
-import NoteStore from '../stores/NoteStore'
+import NoteStore from '../stores/NoteStore.js'
+import Popup from '../components/Popup.vue'
 
 export default {
     props: {
         currentNoteListId: String
     },
 
+    components: {
+        Popup
+    },
+
     data() {
         return {
-            notes: []
+            notes: [],
+            isNoteClicked: false
         }
     },
 
@@ -27,6 +40,18 @@ export default {
         const noteStore = new NoteStore();
         noteStore.getNotesOfANoteListFromDb(this.currentNoteListId)
         this.notes = noteStore.notesOfNoteListFromDb
+    },
+
+    methods: {
+        handleClick(id) {
+            this.isNoteClicked = true
+            console.log(this.isNoteClicked + ' ' + id)
+        },
+
+        handleOutsideClick() {
+            this.isNoteClicked = false
+            console.log(this.isNoteClicked)
+        }
     }
 }
 </script>
---
+++ frontend-vue-interface/trillo-copy/src/components/Popup.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-dialog max-width="600px">
+        <v-card>
+            <v-card-title>
+                <h2>Some title</h2>
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
